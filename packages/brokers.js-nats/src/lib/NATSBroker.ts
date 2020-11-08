e47c7cd28b3dfb186640b6e7305859b8d7a80653
QuantumlyTangled <ethoarx@gmail.com>
2020-11-08 10:36:34 +0100

    feat(brokers.js-nats): initial

---
+++ packages/brokers.js-nats/src/lib/NATSBroker.ts
@@ -0,0 +1,107 @@
+import { Broker, ResponseOptions } from '@byters/brokers.js';
+import { NatsError, Client, connect, RequestOptions, REQ_TIMEOUT } from 'nats';
+
+export interface _NATSSubscription {
+	subject: string;
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	callback: Function;
+	received: number;
+}
+
+export interface NATSBrokerEvents {
+	// #region NATS Errors
+	error: [NatsError];
+	connect: [Client];
+	disconnect: [];
+	reconnecting: [];
+	reconnect: [Client];
+	close: [];
+	unsubscribe: [string | number, string];
+	permission_error: [NatsError];
+	// #endregion NATS Errors
+	// #region Broker Errors
+	callError: [string, NatsError];
+	// #endregion Broker Errors
+}
+
+export class NATSBroker<Send = unknown, Receieve = unknown> extends Broker<Send, Receieve, ResponseOptions> {
+
+	public connection!: Client;
+	private _subscriptions: Map<string, number> = new Map();
+
+	// TODO: Support multiple options
+	public start(): Client {
+		return this.connection = connect();
+	}
+
+	public publish(event: string, data: Send): void {
+		this.connection.publish(event, this.serialize(data));
+	}
+
+	public call(event: string, data: Send, options?: RequestOptions): void {
+		this.connection.requestOne(event, this.serialize(data), options ?? {}, options?.timeout ?? 60000 /* 60s */, (msg: unknown) => {
+			if (msg instanceof NatsError && msg.code === REQ_TIMEOUT) {
+				this.brokerClient.emit('callError', event, msg);
+			} else {
+				this._handleReply(event, msg as Buffer);
+			}
+		});
+	}
+
+	public _subscribe(events: string[]) {
+		return Promise.all(events.map(event => {
+			// https://github.com/nats-io/nats.js/blob/9104791183dcafea7cf3b3180e5c9f61b73b1777/lib/nats.js#L1467
+			// https://docs.nats.io/nats-protocol/nats-protocol#msg
+			const sub = this.connection.subscribe(event, (msg: Buffer, reply: string) => {
+				this._handleMessage(event, msg, {
+					// TODO: Set default serialize method as an extension of `toString`
+					reply: data => this.connection.publish(reply, this.serialize(data as Send))
+				});
+			});
+
+			this._subscriptions.set(event, sub);
+			return sub;
+		}));
+	}
+
+	public _unsubscribe(events: string[]): Promise<boolean[]> {
+		return Promise.all(events.map(event => {
+			const subscription = this._subscriptions.get(event);
+
+			if (subscription === undefined) return false;
+
+			this.connection.unsubscribe(subscription);
+			this._subscriptions.delete(event);
+			return true;
+		}));
+	}
+
+}
+
+declare module '@byters/brokers.js' {
+	export interface Brokers {
+		on<K extends keyof NATSBrokerEvents>(event: K, listener: (...args: NATSBrokerEvents[K]) => void): this;
+		on<S extends string | symbol>(
+			event: Exclude<S, keyof NATSBrokerEvents>,
+			listener: (...args: any[]) => void,
+		): this;
+
+		once<K extends keyof NATSBrokerEvents>(event: K, listener: (...args: NATSBrokerEvents[K]) => void): this;
+		once<S extends string | symbol>(
+			event: Exclude<S, keyof NATSBrokerEvents>,
+			listener: (...args: any[]) => void,
+		): this;
+
+		emit<K extends keyof NATSBrokerEvents>(event: K, ...args: NATSBrokerEvents[K]): boolean;
+		emit<S extends string | symbol>(event: Exclude<S, keyof NATSBrokerEvents>, ...args: any[]): boolean;
+
+		off<K extends keyof NATSBrokerEvents>(event: K, listener: (...args: NATSBrokerEvents[K]) => void): this;
+		off<S extends string | symbol>(
+			event: Exclude<S, keyof NATSBrokerEvents>,
+			listener: (...args: any[]) => void,
+		): this;
+
+		removeAllListeners<K extends keyof NATSBrokerEvents>(event?: K): this;
+		removeAllListeners<S extends string | symbol>(event?: Exclude<S, keyof NATSBrokerEvents>): this;
+	}
+}
